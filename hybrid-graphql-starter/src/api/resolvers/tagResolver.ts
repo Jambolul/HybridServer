35b452111e7eddc70c6302398f69e0a62f828153
jambolul <jami.naatanen@gmail.com>
2024-01-29 21:45:40 +0200

    initial commit

--- hybrid-graphql-starter/src/api/resolvers/tagResolver.ts
+++ hybrid-graphql-starter/src/api/resolvers/tagResolver.ts
@@ -1,24 +1,32 @@
-import { MediaItem, Tag } from '@sharedTypes/DBTypes';
-import {fetchAllTags, fetchTagsByMediaId, postTag} from '../models/tagModel';
+import {Tag} from '@sharedTypes/DBTypes';
+import {
+  deleteTag,
+  fetchAllTags,
+  fetchTagsByMediaId,
+  postTag,
+} from '../models/tagModel';
 
 export default {
   MediaItem: {
     tags: async (parent: {media_id: string}) => {
       console.log(parent);
       return await fetchTagsByMediaId(Number(parent.media_id));
-    }
+    },
   },
-    Query: {
-        tags: async () => {
-            return await fetchAllTags();
-        },
+  Query: {
+    tags: async () => {
+      return await fetchAllTags();
     },
-    Mutation: {
-      createTag: async (
-        _parent: undefined,
-        args: {input: Omit<Tag, 'tag_id'>},
-        ) => {
-          return postTag(args.input);
-        }
+  },
+  Mutation: {
+    createTag: async (
+      _parent: undefined,
+      args: {input: Omit<Tag, 'tag_id'>},
+    ) => {
+      return await postTag(args.input);
     },
+    deleteTag: async (_parent: undefined, args: {input: string}) => {
+      return await deleteTag(Number(args.input));
+    },
+  },
 };
